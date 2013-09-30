0deb11f6a59ec32991beca809b9ff90a4869fa3d
tim h <zero01101@yahoo.com>
2013-09-29 19:14:17 -0500

    it's a metagame

---
+++ glaiel_number/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace glaiel_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblNumberGoUp = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnNumberGoUp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblThisMany = new System.Windows.Forms.Label();
+            this.lblShebang = new System.Windows.Forms.Label();
+            this.lblInstant = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(12, 9);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(126, 46);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblNumberGoUp
+            // 
+            this.lblNumberGoUp.AutoSize = true;
+            this.lblNumberGoUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberGoUp.Location = new System.Drawing.Point(15, 55);
+            this.lblNumberGoUp.Name = "lblNumberGoUp";
+            this.lblNumberGoUp.Size = new System.Drawing.Size(70, 26);
+            this.lblNumberGoUp.TabIndex = 1;
+            this.lblNumberGoUp.Text = "label1";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 107);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnNumberGoUp
+            // 
+            this.btnNumberGoUp.Enabled = false;
+            this.btnNumberGoUp.Location = new System.Drawing.Point(93, 107);
+            this.btnNumberGoUp.Name = "btnNumberGoUp";
+            this.btnNumberGoUp.Size = new System.Drawing.Size(485, 23);
+            this.btnNumberGoUp.TabIndex = 3;
+            this.btnNumberGoUp.Text = "number go up";
+            this.btnNumberGoUp.UseVisualStyleBackColor = true;
+            this.btnNumberGoUp.Click += new System.EventHandler(this.btnNumberGoUp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "it costs this many number to upgrade number go up: ";
+            // 
+            // lblThisMany
+            // 
+            this.lblThisMany.AutoSize = true;
+            this.lblThisMany.Location = new System.Drawing.Point(276, 81);
+            this.lblThisMany.Name = "lblThisMany";
+            this.lblThisMany.Size = new System.Drawing.Size(35, 13);
+            this.lblThisMany.TabIndex = 5;
+            this.lblThisMany.Text = "label2";
+            // 
+            // lblShebang
+            // 
+            this.lblShebang.AutoSize = true;
+            this.lblShebang.Location = new System.Drawing.Point(421, 55);
+            this.lblShebang.Name = "lblShebang";
+            this.lblShebang.Size = new System.Drawing.Size(0, 13);
+            this.lblShebang.TabIndex = 6;
+            // 
+            // lblInstant
+            // 
+            this.lblInstant.AutoSize = true;
+            this.lblInstant.Location = new System.Drawing.Point(421, 81);
+            this.lblInstant.Name = "lblInstant";
+            this.lblInstant.Size = new System.Drawing.Size(0, 13);
+            this.lblInstant.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 142);
+            this.Controls.Add(this.lblInstant);
+            this.Controls.Add(this.lblShebang);
+            this.Controls.Add(this.lblThisMany);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNumberGoUp);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblNumberGoUp);
+            this.Controls.Add(this.lblNumber);
+            this.MaximumSize = new System.Drawing.Size(614, 180);
+            this.MinimumSize = new System.Drawing.Size(614, 180);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblNumberGoUp;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnNumberGoUp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblThisMany;
+        private System.Windows.Forms.Label lblShebang;
+        private System.Windows.Forms.Label lblInstant;
+    }
+}
+
---
+++ glaiel_number/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace glaiel_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double number = 0;
+        double numberGoUp = 0.1;
+        double numberGoUpCost = 10;
+        bool go = false;
+        Stopwatch thewholeshebang = new Stopwatch();
+        Stopwatch instant = new Stopwatch();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            lblNumber.Text = number.ToString();
+            lblNumberGoUp.Text = numberGoUp.ToString() + "x";
+            lblThisMany.Text = numberGoUpCost.ToString();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!(go))
+            {
+                number = 0;
+                numberGoUp = 0.1;
+                numberGoUpCost = 10;
+                go = true;
+                btnGo.Text = "stop";
+                thewholeshebang.Start();
+                instant.Start();
+                timer1.Start();
+            }
+            else if (go)
+            {
+                go = false;
+                btnGo.Text = "go";
+                thewholeshebang.Stop();
+                instant.Stop();
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblInstant.Text = instant.ElapsedMilliseconds.ToString();
+            lblShebang.Text = thewholeshebang.ElapsedMilliseconds.ToString();
+            number += 1.0 * numberGoUp;            
+            lblNumber.Text = Math.Floor(number).ToString();
+            lblThisMany.Text = Math.Floor(numberGoUpCost).ToString();
+            string ngo = numberGoUp.ToString();
+            if (ngo.Length >= 6)
+            {
+                ngo = ngo.Substring(0, 5);
+            }
+            lblNumberGoUp.Text = ngo + "x";
+            if (number > numberGoUpCost)
+            {
+                btnNumberGoUp.Enabled = true;
+            }
+            else
+            {
+                btnNumberGoUp.Enabled = false;
+            }
+        }
+
+        private void btnNumberGoUp_Click(object sender, EventArgs e)
+        {
+            instant.Stop();
+            instant.Reset();
+            number -= Math.Floor(numberGoUpCost);
+            numberGoUp += Math.Sqrt(numberGoUp * 2) / 7;
+            numberGoUpCost += Math.Sqrt(numberGoUpCost * 0.4) * 4.75;
+            btnNumberGoUp.Enabled = false;
+            instant.Start();
+        }
+    }
+}
